42950e2cf88959edec57ee951525c46ae7d2ee0f
Dan <FreePublicWiFi@protonmail.com>
2018-03-29 22:38:14 -0500

    New mining algox

--- src/hash.h
+++ src/hash.h
@@ -346,11 +346,49 @@ uint64_t SipHashUint256Extra(uint64_t k0, uint64_t k1, const uint256& val, uint3
 
 inline int GetHashSelection(const uint256 PrevBlockHash, int index) {
     assert(index >= 0);
-    assert(index < 16);
+    assert(index < 8);
 
-    #define START_OF_LAST_16_NIBBLES_OF_HASH 48
-    int hashSelection = PrevBlockHash.GetNibble(START_OF_LAST_16_NIBBLES_OF_HASH + index);
-    return(hashSelection);
+    #define START_OF_LAST_7_NIBBLES_OF_HASH 57
+    if(index < 7){
+	int hashSelection = PrevBlockHash.GetNibble(START_OF_LAST_7_NIBBLES_OF_HASH + index);
+    	return(hashSelection);
+    }
+    int sumOfSeven = 0;
+    for(int i = 0; i < 7; i++){
+	sumOfSeven += PrevBlockHash.GetNibble(START_OF_LAST_7_NIBBLES_OF_HASH + i);
+    }
+    if(sumOfSeven < 28){
+	return 0;}
+    if(sumOfSeven < 39){
+        return 1;}
+    if(sumOfSeven < 47){
+        return 2;}
+    if(sumOfSeven < 54){
+        return 3;}
+    if(sumOfSeven < 61){
+        return 4;}
+    if(sumOfSeven < 66){
+        return 5;}
+    if(sumOfSeven < 72){
+        return 6;}
+    if(sumOfSeven < 76){
+        return 7;}
+    if(sumOfSeven < 81){
+        return 8;}
+    if(sumOfSeven < 86){
+        return 9;}
+    if(sumOfSeven < 90){
+        return 10;}
+    if(sumOfSeven < 94){
+        return 11;}
+    if(sumOfSeven < 97){
+        return 12;}
+    if(sumOfSeven < 100){
+        return 13;}
+    if(sumOfSeven < 103){
+        return 14;}
+    return 15;
+
 }
 
 extern double algoHashTotal[16];
@@ -363,28 +401,28 @@ inline uint256 HashX16R(const T1 pbegin, const T1 pend, const uint256 PrevBlockH
 //	static std::chrono::duration<double>[16];
     int hashSelection;
 
-    sph_blake512_context     ctx_blake;      //0
+    sph_skein512_context     ctx_skein;      //0
     sph_bmw512_context       ctx_bmw;        //1
-    sph_groestl512_context   ctx_groestl;    //2
-    sph_jh512_context        ctx_jh;         //3
-    sph_keccak512_context    ctx_keccak;     //4
-    sph_skein512_context     ctx_skein;      //5
-    sph_luffa512_context     ctx_luffa;      //6
-    sph_cubehash512_context  ctx_cubehash;   //7
-    sph_shavite512_context   ctx_shavite;    //8
-    sph_simd512_context      ctx_simd;       //9
-    sph_echo512_context      ctx_echo;       //A
-    sph_hamsi512_context     ctx_hamsi;      //B
-    sph_fugue512_context     ctx_fugue;      //C
-    sph_shabal512_context    ctx_shabal;     //D
-    sph_whirlpool_context    ctx_whirlpool;  //E
-    sph_sha512_context       ctx_sha512;     //F
+    sph_blake512_context     ctx_blake;      //2
+    sph_sha512_context       ctx_sha512;     //3
+    sph_shabal512_context    ctx_shabal;     //4
+    sph_keccak512_context    ctx_keccak;     //5
+    sph_shavite512_context   ctx_shavite;    //6
+    sph_whirlpool_context    ctx_whirlpool;  //7
+    sph_cubehash512_context  ctx_cubehash;   //8
+    sph_jh512_context        ctx_jh;         //9
+    sph_luffa512_context     ctx_luffa;      //A
+    sph_echo512_context      ctx_echo;       //B
+    sph_simd512_context      ctx_simd;       //C
+    sph_fugue512_context     ctx_fugue;      //D
+    sph_groestl512_context   ctx_groestl;    //E
+    sph_hamsi512_context     ctx_hamsi;      //F
 
     static unsigned char pblank[1];
 
-    uint512 hash[16];
+    uint512 hash[8];
 
-    for (int i=0;i<16;i++) 
+    for (int i=0;i<8;i++) 
     {
         const void *toHash;
         int lenToHash;
@@ -395,14 +433,13 @@ inline uint256 HashX16R(const T1 pbegin, const T1 pend, const uint256 PrevBlockH
             toHash = static_cast<const void*>(&hash[i-1]);
             lenToHash = 64;
         }
-
         hashSelection = GetHashSelection(PrevBlockHash, i);
 
         switch(hashSelection) {
             case 0:
-                sph_blake512_init(&ctx_blake);
-                sph_blake512 (&ctx_blake, toHash, lenToHash);
-                sph_blake512_close(&ctx_blake, static_cast<void*>(&hash[i]));
+                sph_skein512_init(&ctx_skein);
+                sph_skein512 (&ctx_skein, toHash, lenToHash);
+                sph_skein512_close(&ctx_skein, static_cast<void*>(&hash[i]));
                 break;
             case 1:
                 sph_bmw512_init(&ctx_bmw);
@@ -410,79 +447,79 @@ inline uint256 HashX16R(const T1 pbegin, const T1 pend, const uint256 PrevBlockH
                 sph_bmw512_close(&ctx_bmw, static_cast<void*>(&hash[i]));
                 break;
             case 2:
-                sph_groestl512_init(&ctx_groestl);
-                sph_groestl512 (&ctx_groestl, toHash, lenToHash);
-                sph_groestl512_close(&ctx_groestl, static_cast<void*>(&hash[i]));
+                sph_blake512_init(&ctx_blake);
+                sph_blake512 (&ctx_blake, toHash, lenToHash);
+                sph_blake512_close(&ctx_blake, static_cast<void*>(&hash[i]));
                 break;
             case 3:
-                sph_jh512_init(&ctx_jh);
-                sph_jh512 (&ctx_jh, toHash, lenToHash);
-                sph_jh512_close(&ctx_jh, static_cast<void*>(&hash[i]));
-                break;
-            case 4:
-                sph_keccak512_init(&ctx_keccak);
-                sph_keccak512 (&ctx_keccak, toHash, lenToHash);
-                sph_keccak512_close(&ctx_keccak, static_cast<void*>(&hash[i]));
-                break;
-            case 5:
-                sph_skein512_init(&ctx_skein);
-                sph_skein512 (&ctx_skein, toHash, lenToHash);
-                sph_skein512_close(&ctx_skein, static_cast<void*>(&hash[i]));
-                break;
-            case 6:
-                sph_luffa512_init(&ctx_luffa);
-                sph_luffa512 (&ctx_luffa, toHash, lenToHash);
-                sph_luffa512_close(&ctx_luffa, static_cast<void*>(&hash[i]));
-                break;
-            case 7:
-                sph_cubehash512_init(&ctx_cubehash);
-                sph_cubehash512 (&ctx_cubehash, toHash, lenToHash);
-                sph_cubehash512_close(&ctx_cubehash, static_cast<void*>(&hash[i]));
-                break;
-            case 8:
-                sph_shavite512_init(&ctx_shavite);
-                sph_shavite512(&ctx_shavite, toHash, lenToHash);
-                sph_shavite512_close(&ctx_shavite, static_cast<void*>(&hash[i]));
-                break;
-            case 9:
-                sph_simd512_init(&ctx_simd);
-                sph_simd512 (&ctx_simd, toHash, lenToHash);
-                sph_simd512_close(&ctx_simd, static_cast<void*>(&hash[i]));
-                break;
-            case 10:
-                sph_echo512_init(&ctx_echo);
-                sph_echo512 (&ctx_echo, toHash, lenToHash);
-                sph_echo512_close(&ctx_echo, static_cast<void*>(&hash[i]));
-                break;
-           case 11:
-                sph_hamsi512_init(&ctx_hamsi);
-                sph_hamsi512 (&ctx_hamsi, toHash, lenToHash);
-                sph_hamsi512_close(&ctx_hamsi, static_cast<void*>(&hash[i]));
-                break;
-           case 12:
-                sph_fugue512_init(&ctx_fugue);
-                sph_fugue512 (&ctx_fugue, toHash, lenToHash);
-                sph_fugue512_close(&ctx_fugue, static_cast<void*>(&hash[i]));
-                break;
-           case 13:
-                sph_shabal512_init(&ctx_shabal);
-                sph_shabal512 (&ctx_shabal, toHash, lenToHash);
-                sph_shabal512_close(&ctx_shabal, static_cast<void*>(&hash[i]));
-                break;
-           case 14:
-                sph_whirlpool_init(&ctx_whirlpool);
-                sph_whirlpool(&ctx_whirlpool, toHash, lenToHash);
-                sph_whirlpool_close(&ctx_whirlpool, static_cast<void*>(&hash[i]));
-                break;
-           case 15:
                 sph_sha512_init(&ctx_sha512);
                 sph_sha512 (&ctx_sha512, toHash, lenToHash);
                 sph_sha512_close(&ctx_sha512, static_cast<void*>(&hash[i]));
                 break;
+            case 4:
+                sph_shabal512_init(&ctx_shabal);
+                sph_shabal512 (&ctx_shabal, toHash, lenToHash);
+                sph_shabal512_close(&ctx_shabal, static_cast<void*>(&hash[i]));
+                break;
+            case 5:
+                sph_keccak512_init(&ctx_keccak);
+                sph_keccak512 (&ctx_keccak, toHash, lenToHash);
+                sph_keccak512_close(&ctx_keccak, static_cast<void*>(&hash[i]));
+                break;
+            case 6:
+                sph_shavite512_init(&ctx_shavite);
+                sph_shavite512(&ctx_shavite, toHash, lenToHash);
+                sph_shavite512_close(&ctx_shavite, static_cast<void*>(&hash[i]));
+                break;
+            case 7:
+                sph_whirlpool_init(&ctx_whirlpool);
+                sph_whirlpool(&ctx_whirlpool, toHash, lenToHash);
+                sph_whirlpool_close(&ctx_whirlpool, static_cast<void*>(&hash[i]));
+                break;
+            case 8:
+                sph_cubehash512_init(&ctx_cubehash);
+                sph_cubehash512 (&ctx_cubehash, toHash, lenToHash);
+                sph_cubehash512_close(&ctx_cubehash, static_cast<void*>(&hash[i]));
+                break;
+            case 9:
+                sph_jh512_init(&ctx_jh);
+                sph_jh512 (&ctx_jh, toHash, lenToHash);
+                sph_jh512_close(&ctx_jh, static_cast<void*>(&hash[i]));
+                break;
+            case 10:
+                sph_luffa512_init(&ctx_luffa);
+                sph_luffa512 (&ctx_luffa, toHash, lenToHash);
+                sph_luffa512_close(&ctx_luffa, static_cast<void*>(&hash[i]));
+                break;
+            case 11:
+                sph_echo512_init(&ctx_echo);
+                sph_echo512 (&ctx_echo, toHash, lenToHash);
+                sph_echo512_close(&ctx_echo, static_cast<void*>(&hash[i]));
+                break;
+            case 12:
+                sph_simd512_init(&ctx_simd);
+                sph_simd512 (&ctx_simd, toHash, lenToHash);
+                sph_simd512_close(&ctx_simd, static_cast<void*>(&hash[i]));
+                break;
+            case 13:
+                sph_fugue512_init(&ctx_fugue);
+                sph_fugue512 (&ctx_fugue, toHash, lenToHash);
+                sph_fugue512_close(&ctx_fugue, static_cast<void*>(&hash[i]));
+                break;
+            case 14:
+                sph_groestl512_init(&ctx_groestl);
+                sph_groestl512 (&ctx_groestl, toHash, lenToHash);
+                sph_groestl512_close(&ctx_groestl, static_cast<void*>(&hash[i]));
+                break;
+            case 15:
+                sph_hamsi512_init(&ctx_hamsi);
+                sph_hamsi512 (&ctx_hamsi, toHash, lenToHash);
+                sph_hamsi512_close(&ctx_hamsi, static_cast<void*>(&hash[i]));
+                break;
         }
     }
 
-    return hash[15].trim256();
+    return hash[7].trim256();
 }
 
 
